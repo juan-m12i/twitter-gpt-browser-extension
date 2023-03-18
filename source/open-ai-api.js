0c0db1e481743dc6d954e247c677a1db7ef0bf47
Juan <juan@tymit.com>
2023-03-18 20:34:28 -0300

    added jsdoc to open ai

--- source/open-ai-api.js
+++ source/open-ai-api.js
@@ -2,6 +2,13 @@ const OPEN_AI_API_URL = "https://api.openai.com/v1/chat/completions";
 
 console.log('open-ai-api.js loaded');
 
+/**
+ * Fetches a response from the OpenAI API.
+ *
+ * @param {Array<{ role: string, content: string }>} messages - The array of messages to send to the API.
+ * @returns {Promise<string>} - The content of the AI's response.
+ * @throws {Error} - Throws an error if the API request fails.
+ */
 async function fetchOpenAIResponse(messages) {
   const response = await fetch(OPEN_AI_API_URL, {
     method: "POST",
